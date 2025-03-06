f73de591ab7e76e423162ea445de2eb5a8b558fa
Zachary White <zachwhite2001@gmail.com>
2025-03-05 21:19:29 -0500

    add custom changes to HTML, CSS, and Javascript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // create an array of sounds
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong','oiia'];
+const oiiaGif = document.getElementById('oiiaGif');
 
 // For each sound, create a button, add a class "btn", update text of the button, append to DOM
 sounds.forEach((sound) => {
@@ -18,12 +19,28 @@ sounds.forEach((sound) => {
 
         // stop all sounds first
         stopSounds();
+
+        // get audio element...
+        const audio = document.getElementById(sound);
         // play sound  on click
-        document.getElementById(sound).play();
+        audio.play();
+        
+        // if my sound is the special oiia
+        if (sound === "oiia") {
+            // make the gif visible
+            oiiaGif.style.display = "block";
+
+            // Force gif to reload itself so that I get consistent "animation"
+            oiiaGif.src = oiiaGif.src;
+
+            // Hide the GIF when the audio finishes
+            audio.onended = () => {
+                oiiaGif.style.display = "none";
+            };
+        }
     });
     // add each button to the DOM
     document.getElementById('buttons').appendChild(btn);
-
 });
 
 // Helper Functions
@@ -41,4 +58,7 @@ function stopSounds() {
         // Reset time of sound to 0
         song.currentTime = 0;
     });
+
+    // Hide my special gif if sound is stopped
+    oiiaGif.style.display = "none";
 }
